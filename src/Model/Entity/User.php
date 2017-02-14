7a5a8bed7f3c07e54d24327c374ac37d4f1fa108
vanessasoutoc <vanessasoutoc@gmail.com>
2017-02-14 16:30:59 -0200

    #2 - usando criptografia na senha do usuario

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -2,6 +2,7 @@
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Cake\Auth\DefaultPasswordHasher;
 
 /**
  * User Entity
@@ -26,6 +27,13 @@ class User extends Entity
      *
      * @var array
      */
+    
+    protected function _setPassword($value)
+    {
+        $hasher = new DefaultPasswordHasher();
+        return $hasher->hash($value);
+    }
+    
     protected $_accessible = [
         '*' => true,
         'id' => false
